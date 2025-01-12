49cce1def117bf15198b73b9c8a76a06b99dc51a
Swapnil5390 <pawar3382@gmail.com>
2025-01-12 15:41:45 +0530

    Dependency Injection(Singleton Added)

--- DependancyPractice/Program.cs
+++ DependancyPractice/Program.cs
@@ -1,18 +1,36 @@
-﻿namespace DependancyPractice
+﻿
+using Microsoft.Extensions.DependencyInjection;
+namespace DependancyPractice
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //  Console.WriteLine("Hello, World!");
-            IDataAccess dataAccessNosql =new DataAccessNosql();
-            IDataAccess sql =new DataAccessSql();
-            IDataAccess redis = new Redis();
-          IBussiness obj = new Bussiness(redis);//
-            IBussiness obj2 = new Bussiness2(sql);
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton<IDataAccess,DataAccessNosql>();
+             services.AddSingleton<IDataAccess, DataAccessSql>();
+            services.AddSingleton<IDataAccess, Redis>();
+            services.AddSingleton<IBussiness, Bussiness>();
+            services.AddSingleton<IBussiness, Bussiness2>();
+            services.AddSingleton<UserInterface>();
 
-            UserInterface userInterface = new UserInterface(obj2);
-            userInterface.getData();
+            IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            
+            UserInterface ui=serviceProvider.GetService<UserInterface>();
+
+            ui.getData();
+
+
+            //  Console.WriteLine("Hello, World!");
+            //  IDataAccess dataAccessNosql =new DataAccessNosql();
+            //  IDataAccess sql =new DataAccessSql();
+            //  IDataAccess redis = new Redis();
+            //IBussiness obj = new Bussiness(redis);//
+            //  IBussiness obj2 = new Bussiness2(sql);
+
+            //  UserInterface userInterface = new UserInterface(obj2);
+            //  userInterface.getData();
         }
     }
 }
